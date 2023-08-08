1f00d7be9cdd787634cfb8ddea0e79f221d0081c
sitogi <takano162@gmail.com>
2023-08-08 22:12:01 +0900

    Add get data from Deno KV

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
 import { Hono } from 'https://deno.land/x/hono@v3.3.1/mod.ts'
 
 const app = new Hono()
+const kv = await Deno.openKv();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!');
+app.get('/', async (c) => {
+  const res = await kv.get(['users', 'alice'])
+  console.log(res.key);
+  console.log(res.value);
+
+  return c.json(res.value);
 });
 
 Deno.serve(app.fetch)
